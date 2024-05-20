d41ed240d32522bcd8c10a93930325c45fffee42
Fubara <theophilus.dickson13@gmail.com>
2024-05-20 15:08:24 +0100

    Tailwind Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section>
+        <p className="h-screen bg-purple-600 w-full text-white flex items-center justify-center m-12 text-center text-3xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, qui
+          porro voluptas quasi animi minus iusto sint iure quis veniam aliquid
+          itaque mollitia nam libero modi repellendus consequuntur
+          exercitationem suscipit.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default App;
